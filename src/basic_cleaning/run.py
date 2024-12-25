685af71665655aa0e441e2d6959619397e1e161c
Christophe Bourgoin <ChrisBg@users.noreply.github.com>
2024-12-25 12:14:15 +0100

    Added a new basic_cleaning stet to remove outliers in longitude & latitude

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -37,6 +37,9 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # Remove outliers in longitude and latitude
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     logger.info("Saving cleaned data")
